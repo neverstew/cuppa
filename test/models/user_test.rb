73d529413b747e6abbff0d70ae80b39df4fd5933
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-27 20:10:24 +0000

    Users can have role

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can be seeker' do
+    user = User.new
+    user.role = :seeker
+
+    assert user.seeker?
+  end
+
+  test 'can be helper' do
+    user = User.new
+    user.role = :helper
+
+    assert user.helper?
+  end
 end
