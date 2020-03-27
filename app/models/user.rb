78494081fb04e3f4c3a65521e345a09f47aaa7c8
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-27 20:33:49 +0000

    User chooses role on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: [:seeker, :helper]
+
+  validates_presence_of :role
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test 'can be created' do
+    user = User.create(
+      id: 1,
+      email: 'somemail@example.com',
+      password: 'mygreatpassword',
+      created_at: Time.now(),
+      updated_at: Time.now(),
+      role: 0
+    )
+
+    assert user.persisted?
+  end
+
+  test 'cannot be created without role' do
+    user = User.create(
+      id: 1,
+      email: 'somemail@example.com',
+      password: 'mygreatpassword',
+      created_at: Time.now(),
+      updated_at: Time.now(),
+      # role: 0
+    )
+
+    assert !user.persisted?
+  end
+
   test 'can be seeker' do
     user = User.new
     user.role = :seeker
