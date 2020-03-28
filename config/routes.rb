ab793e68998ca935bf96063ccd7e01d0b4e76e85
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-28 16:51:43 +0000

    Configure matches as resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'matches/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#index'
   
@@ -7,4 +6,5 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users, only: [:show, :edit]
+  resources :matches, only: [:index]
 end
