7fe939f14dc653f7f57ee01334d785e51009782e
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-28 23:20:59 +0000

    Simple display of matches on matches#index

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,5 +5,9 @@ class MatchesController < ApplicationController
     unless current_user.completed_onboarding_at? then
       redirect_to edit_user_registration_path and return
     end
+
+    if current_user.seeker? then
+      @matches = User.helper.available.limit(100).select{ |user| user.within(5000, current_user) }
+    end
   end
 end
