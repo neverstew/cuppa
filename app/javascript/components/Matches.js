1681f42250b399a9f0f558def2189879e34382b9
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-29 20:41:08 +0100

    Don't show the whole matches tab section if helper
    They can't navigate anywhere anyway, so no point showing the tabs.

--- app/javascript/components/Matches.js
+++ app/javascript/components/Matches.js
@@ -1,42 +1,51 @@
 import React, { useState, useEffect } from "react";
 import MatchesCarousel from "./MatchesCarousel";
-import MatchListWrapper from './MatchList';
+import MatchListWrapper from "./MatchList";
 
 const Matches = ({ active, currentMatches, upcomingMatches }) => {
-  if (active === 'upcoming') {
+  if (active === "upcoming") {
     return <MatchesCarousel matches={upcomingMatches} />;
   }
 
   return <MatchListWrapper matches={currentMatches} />;
 };
 
-const MatchesTabs = ({ active, navigate, showUpcoming }) => {
-  const activeClass = tab => active === tab ? 'btn-dark text-light' : 'btn-light';
+const MatchesTabs = ({ active, navigate }) => {
+  const activeClass = tab =>
+    active === tab ? "btn-dark text-light" : "btn-light";
 
   return (
-    <div className='row ml-0 mr-0'>
-      <div className='column col-6 pl-0 pr-0'>
-        <a className={`w-100 btn pt-2 pr-4 pb-2 pl-4 ${activeClass('current')}`} onClick={() => navigate('current')}>Current</a>
+    <div className="row ml-0 mr-0">
+      <div className="column col-6 pl-0 pr-0">
+        <a
+          className={`w-100 btn pt-2 pr-4 pb-2 pl-4 ${activeClass("current")}`}
+          onClick={() => navigate("current")}
+        >
+          Current
+        </a>
       </div>
-      { showUpcoming && (
-        <div className='column col-6 pl-0 pr-0'>
-          <a className={`w-100 btn pt-2 pr-4 pb-2 pl-4 ${activeClass('upcoming')}`} onClick={() => navigate('upcoming')}>New</a>
-        </div>
-      )}
+      <div className="column col-6 pl-0 pr-0">
+        <a
+          className={`w-100 btn pt-2 pr-4 pb-2 pl-4 ${activeClass("upcoming")}`}
+          onClick={() => navigate("upcoming")}
+        >
+          New
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-const withMatchesLayout = (WrappedComponent) => props => {
-  const [tab, navigate] = useState('current');
+const withMatchesLayout = WrappedComponent => props => {
+  const [tab, navigate] = useState("current");
 
   return (
     <div>
       <h1>Matches</h1>
-      <MatchesTabs active={tab} navigate={navigate} showUpcoming={props.showUpcoming}/>
+      {props.showUpcoming && <MatchesTabs active={tab} navigate={navigate} />}
       <WrappedComponent active={tab} {...props} />
     </div>
   );
-}
+};
 
 export default withMatchesLayout(Matches);
