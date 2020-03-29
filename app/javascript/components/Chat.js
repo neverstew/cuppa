1a1af8df3f2b22904a521e9a0561b0c8ee2ed079
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-29 18:22:31 +0100

    Matches page has rudimentary tabs

---
+++ app/javascript/components/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Chat = () => {
+  return <p>Chat page</p>
+}
+
+export default Chat;
--- app/javascript/components/Matches.js
+++ app/javascript/components/Matches.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import MatchesCarousel from "./MatchesCarousel";
+import Chat from './Chat';
 
-const Matches = ({ currentMatches, upcomingMatches }) => {
-  return <MatchesCarousel matches={upcomingMatches} />;
+const Matches = ({ active, currentMatches, upcomingMatches }) => {
+  if (active === 'upcoming') {
+    return <MatchesCarousel matches={upcomingMatches} />;
+  }
+
+  return <Chat matches={currentMatches} />;
 };
 
+const MatchesTabs = ({ active, navigate }) => {
+  const activeClass = tab => active === tab ? 'btn-dark text-light' : 'btn-light';
+
+  return (
+    <div className='row ml-1'>
+      <div className='column col-6 pl-0 pr-0'>
+        <a className={`w-100 btn pt-2 pr-4 pb-2 pl-4 ${activeClass('current')}`} onClick={() => navigate('current')}>Current</a>
+      </div>
+      <div className='column col-6 pl-0 pr-0'>
+        <a className={`w-100 btn pt-2 pr-4 pb-2 pl-4 ${activeClass('upcoming')}`} onClick={() => navigate('upcoming')}>New</a>
+      </div>
+    </div>
+  )
+}
+
 const withMatchesLayout = (WrappedComponent) => props => {
+  const [tab, navigate] = useState('upcoming');
+
   return (
     <div>
       <h1>Matches</h1>
-      <WrappedComponent {...props} />
+      <MatchesTabs active={tab} navigate={navigate} />
+      <WrappedComponent active={tab} {...props} />
     </div>
   );
 }
